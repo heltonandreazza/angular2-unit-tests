ea1ca7647c1b019a11bb0353ecedba9d1693c6e3
Helton Andreazza <helton.prg@gmail.com>
2017-05-31 22:51:51 -0300

    improve describe pipe test

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { ReversePipe } from "app/shared/reverse.pipe";
 // THIS IS A ISOLATED TEST(it has no depedency except for itself)
-describe('Pipe', () => {
+describe('Pipe: ReversePipe', () => {
   it('should create the app', () => {
     let reversePipe = new ReversePipe();
     expect(reversePipe.transform('hello')).toEqual('olleh');
