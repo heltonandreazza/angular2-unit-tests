29bf041c62702a2c9e735403527ae673bddf14be
Helton Andreazza <helton.prg@gmail.com>
2017-05-31 22:50:43 -0300

    change doc

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -1,5 +1,5 @@
 import { ReversePipe } from "app/shared/reverse.pipe";
-// THIS IS A ISOLATED TESTE, TO MAKE A NON- ISOLATED TEST I COUDL CREATE A TEST USING THIS PIPE AS A DEPENCY OF USER COMPONENT FOR INSTANCE
+// THIS IS A ISOLATED TEST(it has no depedency except for itself)
 describe('Pipe', () => {
   it('should create the app', () => {
     let reversePipe = new ReversePipe();
