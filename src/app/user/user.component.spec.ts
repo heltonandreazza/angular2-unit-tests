24a8961f8f4680fddc4b32edd3e0614346cbca55
Helton Andreazza <helton.prg@gmail.com>
2017-05-31 22:27:11 -0300

    add async services testes with async and fakeasync aproaches

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,15 +1,17 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { UserComponent } from './user.component';
 import { UserService } from "app/user/user.service";
+import { DataService } from "app/shared/data.service";
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
   let userService: UserService;
+  let dataService: DataService;
   let compiled: any;
 
   beforeEach(async(() => {
@@ -22,9 +24,7 @@ describe('UserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
-    userService = fixture.debugElement.injector.get(UserService);
     compiled = fixture.debugElement.nativeElement;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -32,6 +32,8 @@ describe('UserComponent', () => {
   });
 
   it('should use the user name from the service', () => {
+    userService = fixture.debugElement.injector.get(UserService);
+    fixture.detectChanges();
     expect(userService.user.name).toEqual(component.user.name);
   })
 
@@ -46,4 +48,39 @@ describe('UserComponent', () => {
     fixture.detectChanges();
     expect(compiled.querySelector('p').textContent).toContain('Please log in first');
   })
+
+  it('shouldn\'t fetch data successfully if not called async', () => {
+    dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('FakeData'));
+    fixture.detectChanges();
+    expect(component.data).toBe(undefined);
+  })
+
+  //P.S - async and fakeasync is basically the same time and results, but with diferent philosofies
+  it('should fetch data successfully if called async', async(() => {
+    dataService = fixture.debugElement.injector.get(DataService);
+    //override the dataService.getDetails() async method returning a fake data when resolved
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('FakeData'));
+    fixture.detectChanges();
+    //when the async resolve promise is finish
+    fixture.whenStable().then(() => {
+      expect(component.data).toBe('FakeData');
+    })
+  }))
+
+  //P.S - async and fakeasync is basically the same time and results, but with diferent philosofies
+  it('should fetch data successfully if called async', fakeAsync(() => {
+    dataService = fixture.debugElement.injector.get(DataService);
+    //override the dataService.getDetails() async method returning a fake data when resolved
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('FakeData'));
+    fixture.detectChanges();
+    //when the async resolve promise is finish
+    //with fakeAsync I can remove the fixture.whenStable().then(() => expect(component.data).toBe('FakeData');)
+    tick(); //tick basically means: in a fake async envoriment, finish all the async tasks/calls(spy) now
+    expect(component.data).toBe('FakeData');
+  }))
+
 });
